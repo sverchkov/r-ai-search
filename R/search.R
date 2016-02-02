4001a5ae9a2fd32281fb6faf7d1757a09fc10442
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2016-02-02 13:02:40 -0600

    Fully working branch-and-bound, untested

--- R/search.R
+++ R/search.R
@@ -5,13 +5,56 @@
 #' This function assumes that the search space is a tree, meaning that it does not
 #' check whether it is revisiting a node.
 #'
+#' We use the typical convention of the score representing a cost, meaning that lower
+#' scores are better, and the optimum is a minimum.
+#'
 #' @param init.node Initial node of the search space
-#' @param getScoreBounds Function that returns the upper and lower bounds of the score
+#' @param getCostBounds Function that returns the upper and lower bounds of the cost
 #' given a node
 #' @param getChildren Function that gets the children of the current node in the search
 #' tree
 #' @param isGoal Function that determines whether a node is a goal node
 #' @return an optimal goal node
-branchBoundOnTree = function( init.node, getScoreBounds, getChildren, isGoal ){
-  # return
+branchBoundOnTree = function( init.node, getCostBounds, getChildren, isGoal ){
+
+  # Node queue
+  queue = list()
+  # Cost queue
+  costs = vector( mode = numeric )
+  # Upper bound: anything with a lowerbound above this gets thrown out
+  highest.cost = Inf
+
+  # Initialize the search
+  current.node = init.node
+
+  while ( !isGoal( current.node ) ) {
+
+    # Branch out
+    for ( child in getChildren( current.node ) ){
+
+      bounds = getCostBounds( child )
+
+      # Bound check for pruning
+      if ( bounds["lower"] >= highest.cost ){
+
+        # Update the highest.cost
+        if ( bounds["upper"] < highest.cost )
+          highest.cost = bounds["upper"]
+
+        # Insert into queues
+        queue = c( queue, list( child ) )
+        costs = c( costs, bounds["lower"] )
+      }
+    }
+
+    # Pop next node from queue
+    optima = which( costs = min( costs ) )
+    index = optima[ length[ optima ] ]
+    current.node = queue[[ index ]]
+    costs = costs[ -index ]
+    queue = queue[ -index ]
+  }
+
+  # Return
+  current.node
 }
